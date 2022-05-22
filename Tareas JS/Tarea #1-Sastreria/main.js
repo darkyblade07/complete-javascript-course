d23a027f05ea495c11e13a1ff4fedd1cc67a2735
darkyblade07 <101464523+darkyblade07@users.noreply.github.com>
2022-05-22 18:12:38 -0500

    Uploading files

---
+++ Tareas JS/Tarea #1-Sastreria/main.js
@@ -0,0 +1,17 @@
+window.onload(){
+
+    var cantidadDePersonasIn = document.getElementById('cantidadDePersonasIn');
+    var cantidadDeTelasPorPersonasIn = document.getElementById('cantidadDeTelasPorPersonasIn');
+    var precioDeLaTela = document.getElementById('precioDeLaTela');
+    var calcularBtn = document.getElementById('calcularBtn');
+    var resultadoLbl = document.getElementById('resultadoLbl');
+
+calcularBtn.onclick=onCalcularBtn;
+
+
+
+function onCalcularBbn (){
+
+}
+
+}
