5233aeeef55d10dc581248f3c5403a1f6ed86c62
darkyblade07 <101464523+darkyblade07@users.noreply.github.com>
2022-05-22 20:48:54 -0500

    Tarea sastreria v.1

--- Tareas JS/Tarea #1-Sastreria/main.js
+++ Tareas JS/Tarea #1-Sastreria/main.js
@@ -1,16 +1,21 @@
 window.onload(){
 
-    var cantidadDePersonasIn = document.getElementById('cantidadDePersonasIn');
+    var cantidadDePersonasIn = document.getElementById('cantidadDePersonasIn')
     var cantidadDeTelasPorPersonasIn = document.getElementById('cantidadDeTelasPorPersonasIn');
-    var precioDeLaTela = document.getElementById('precioDeLaTela');
     var calcularBtn = document.getElementById('calcularBtn');
     var resultadoLbl = document.getElementById('resultadoLbl');
 
-calcularBtn.onclick=onCalcularBtn;
+calcularBtn.onclick=calcularBtn;
 
 
 
-function onCalcularBbn (){
+function calcularBtn (){
+var cantidadDePersonas=Number(cantidadDeTelasPorPersonasIn.value);
+var cantidadDeTelasPorPersonasIn=Number(cantidadDeTelasPorPersonasIn.value);
+var precioDeLaTela=3550;
+var precioFinal= (precioDeLaTela*cantidadDeTelasPorPersonasIn);
+resultadoLbl.innerHTML='Precio final: ' +precioFinal.toFixed(2);
+
 
 }
 
