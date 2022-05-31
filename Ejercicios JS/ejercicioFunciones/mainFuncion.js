c077879dbb0cae16394199a13f267327521977a5
darkyblade07 <101464523+darkyblade07@users.noreply.github.com>
2022-05-30 21:56:00 -0500

    function

---
+++ Ejercicios JS/ejercicioFunciones/mainFuncion.js
@@ -0,0 +1,22 @@
+function imprimirEnConsola() {
+    console.log('imprimirEnConsola');
+}
+
+imprimirEnConsola();
+imprimirEnConsola();
+imprimirEnConsola();
+imprimirEnConsola();
+imprimirEnConsola();
+imprimirEnConsola();
+imprimirEnConsola();
+imprimirEnConsola();
+imprimirEnConsola();
+
+
+function imprimirEnConsola2(msj) {
+
+    console.log(msj);
+}
+
+imprimirEnConsola2('hola');
+imprimirEnConsola2('Cm estas');
