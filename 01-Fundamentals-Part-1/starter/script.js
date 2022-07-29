0240a1efe83907619ef69e86277a0eb268540dfe
darkyblade07 <101464523+darkyblade07@users.noreply.github.com>
2022-07-28 21:07:30 -0500

    dd

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,18 @@
+// let country = 'Costa Rica';
+// let continent = 'America';
+// let population = 55552;
+
+// console.log(`Country: ${country}, Continent: ${continent} and Population: ${population} `)
+
+// let markWeight = 78;
+// let markHeight = 1.69;
+// let johnWeight = 92;
+// let johnHeight = 1.95;
+
+// const markBmi =  markWeight / (markHeight **2);
+// const johnBmi =  johnWeight / (johnHeight **2);
+
+// const markHigherBMI = markBmi > johnBmi;
+
+const inputYear = '1991';
+console.log(Number(inputYear) + 18) 
